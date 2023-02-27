e925a6a6c1908789345ffc84b44657f0e1692ddc
vlad <vnelipovich63@gmail.com>
2023-02-27 20:26:47 +0300

    use controlled and uncontrolled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Accordion from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
-import OnOff from "./components/OnOff/OnOff";
-import UncontrolledAccordion from "./components/UncontrolledAccordion/UncontrolledAccordion";
+import {Rating, RatingValueType} from "./components/Rating/Rating";
 import UncontrolledRating from "./components/UncontrolledRating/UncontrolledRating";
+import Accordion from "./components/Accordion/Accordion";
+import OnOff from "./components/OnOff/OnOff";
+import UncontrolledOnOff from "./components/UncontrolledOnOff/UncontrolledOnOff";
 
 
 function App() {
+
+    let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+    let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
+    let [switchOn, setSwitchOn] = useState<boolean>(false)
+
     console.log("App rendering")
     return (
         <div className="App">
-            <OnOff />
+            <Rating value={ratingValue} onClick={setRatingValue}/>
+            <UncontrolledRating/>
+            {/*<Rating value={3}/>*/}
+            <Accordion titleValue={"Menu"} collapsed={accordionCollapsed} onChange={() => {setAccordionCollapsed(!accordionCollapsed)} }/>
+            {/*<OnOff on={switchOn} onChange={setSwitchOn}/>*/}
 
-            <UncontrolledAccordion titleValue={"Menu"} />
-            <Accordion titleValue={"Menu"} collapsed={false}/>
-
-            <UncontrolledRating />
-            <Rating value={3}/>
+            <UncontrolledOnOff onChange={setSwitchOn}/> {switchOn.toString()}
 
         </div>
     );
--- src/components/UncontrolledOnOff/UncontrolledOnOff.tsx
+++ src/components/UncontrolledOnOff/UncontrolledOnOff.tsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 
 type OnOffPropsType = {
-     on: boolean
-    onChange: ( on: boolean )=> void
+    onChange: (on: boolean) => void
 }
 
-const OnOff: React.FC<OnOffPropsType> = (props) => {
+const UncontrolledOnOff: React.FC<OnOffPropsType> = (props) => {
+
+    let [on, setOn] = useState(false);
 
     const onStyle = {
         width: "30px",
@@ -13,7 +14,7 @@ const OnOff: React.FC<OnOffPropsType> = (props) => {
         border: "1px solid black",
         display: "inline-block",
         padding: "2px",
-        backgroundColor: props.on ? "green" : "white"
+        backgroundColor: on ? "green" : "white"
     }
     const offStyle = {
         width: "30px",
@@ -22,7 +23,7 @@ const OnOff: React.FC<OnOffPropsType> = (props) => {
         display: "inline-block",
         marginLeft: "2px",
         padding: "2px",
-        backgroundColor: props.on ? "white" : "red"
+        backgroundColor: on ? "white" : "red"
     }
     const indicatorStyle = {
         width: "10px",
@@ -31,17 +32,26 @@ const OnOff: React.FC<OnOffPropsType> = (props) => {
         border: "1px solid black",
         display: "inline-block",
         marginLeft: "5px",
-        backgroundColor: props.on ? "green" : "red"
+        backgroundColor: on ? "green" : "red"
     }
+    const onClicked = () => {
+        setOn(true)
+        props.onChange(true)
+    }
+    const offClicked = () => {
+        setOn(false)
+        props.onChange(false)
+    }
+
 
     return (
         <div>
-            <div style={onStyle} onClick={ () => { props.onChange(true) }} >On</div>
-            <div style={offStyle} onClick={ () => { props.onChange(false) }} >Off</div>
+            <div style={onStyle} onClick={onClicked}>On</div>
+            <div style={offStyle} onClick={offClicked}>Off</div>
             <div style={indicatorStyle}></div>
         </div>
     );
 };
 
 
-export default OnOff;
+export default UncontrolledOnOff;
